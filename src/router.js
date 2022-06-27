8f556dbcef23405fcaf33dd7de742f46edfbc25d
João Pedro Marinho <marinhojp@outloo.com>
2022-06-27 15:32:05 -0300

    Add code to router

--- src/router.js
+++ src/router.js
@@ -2,13 +2,38 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Layout from "./components/Layout/";
+import Courses from "./pages/Courses";
+// import Allocation from "./pages/Allocation";
+// import Professor from "./pages/Professor";
+// import Department from "./pages/Department";
 
 const routes = [{
     path: "/",
     name: "Home",
     visible: false,
-    Component: Home,
-}];
+    component: Home,
+},
+{
+    path: "/courses",
+    name: "Courses",
+    component: Courses,
+},
+// {
+//     path: "/professor",
+//     name: "Professor",
+//     component: Professor,
+// },
+// {
+//     path: "/department",
+//     name: "Department",
+//     component: Department,
+// },
+// {
+//     path: "/allocations",
+//     name: "Allocation",
+//     component: Allocation,
+// },
+];
 
 const Router = () => (
 <BrowserRouter>
