338603f59c416a1e16887108098882bf6e63d606
João Pedro Marinho <marinhojp@outloo.com>
2022-06-27 21:54:28 -0300

    Removed eslint line

--- src/components/ListView/index.js
+++ src/components/ListView/index.js
@@ -24,7 +24,6 @@ const ListView = ({ actions, children, columns, endpoint }) => {
 
   useEffect(() => {
     getData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
