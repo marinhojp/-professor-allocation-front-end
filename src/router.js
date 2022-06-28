1c3b9e1cc478bd99283ce7e5fbfe8c68e4a1a555
João Pedro Marinho <marinhojp@outloo.com>
2022-06-28 15:24:51 -0300

    Changed departments route

--- src/router.js
+++ src/router.js
@@ -5,51 +5,52 @@ import Layout from "./components/Layout/";
 import Courses from "./pages/Courses";
 // import Allocation from "./pages/Allocation";
 // import Professor from "./pages/Professor";
-// import Department from "./pages/Department";
+import Department from "./pages/Department";
 
-const routes = [{
+const routes = [
+  {
     path: "/",
     name: "Home",
     visible: false,
     component: Home,
-},
-{
+  },
+  {
     path: "/courses",
     name: "Courses",
     component: Courses,
-},
-// {
-//     path: "/professor",
-//     name: "Professor",
-//     component: Professor,
-// },
-// {
-//     path: "/department",
-//     name: "Department",
-//     component: Department,
-// },
-// {
-//     path: "/allocations",
-//     name: "Allocation",
-//     component: Allocation,
-// },
+  },
+  // {
+  //     path: "/professor",
+  //     name: "Professor",
+  //     component: Professor,
+  // },
+  {
+    path: "/departments",
+    name: "Department",
+    component: Department,
+  },
+  // {
+  //     path: "/allocations",
+  //     name: "Allocation",
+  //     component: Allocation,
+  // },
 ];
 
 const Router = () => (
-<BrowserRouter>
-<Layout routes ={routes}>
-    <Switch>
+  <BrowserRouter>
+    <Layout routes={routes}>
+      <Switch>
         {routes.map((route, index) => (
-            <Route
-                component={route.component}
-                exact
-                key={index}
-                path={route.path}
-            />
-        ))}        
-    </Switch>
-</Layout>
-</BrowserRouter>
+          <Route
+            component={route.component}
+            exact
+            key={index}
+            path={route.path}
+          />
+        ))}
+      </Switch>
+    </Layout>
+  </BrowserRouter>
 );
 
 export default Router;
